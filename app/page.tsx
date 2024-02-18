cfb1695bfb90bcf396064aac692172ad6a1b8738
Van <wanniejvr08@gmail.com>
2024-02-18 21:21:51 +0200

    UX(styles)

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { CollaborativeApp } from "./CollaborativeApp";
 export default function Page() {
   return (
     <Room>
-      <h1 className="font-xl border-2">Testing</h1>
+      <h1 className="text-xl">Testing</h1>
       <CollaborativeApp />
     </Room>
   );
