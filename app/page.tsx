708f9f8b01e9f5fe8f52e849271d8e9afe08ed9f
Van <wanniejvr08@gmail.com>
2024-02-18 21:29:18 +0200

    chore(liveblocks): room to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { Room } from "./Room";
 import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
-    <Room>
+    <div>
       <h1 className="text-xl">Testing</h1>
       <CollaborativeApp />
-    </Room>
+    </div>
   );
 }
