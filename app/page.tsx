0f93110534f63252fdae02387119ce6b24f6d0ea
Van <wanniejvr08@gmail.com>
2024-02-18 21:19:22 +0200

    UX(shadcn)

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { CollaborativeApp } from "./CollaborativeApp";
 export default function Page() {
   return (
     <Room>
+      <h1 className="font-xl border-2">Testing</h1>
       <CollaborativeApp />
     </Room>
   );
